ab8757d5ea2d080b4b4e6e7a3782139dfab6cc90
Jon Akutagawa <jakutagawa@cox.net>
2017-03-09 17:53:10 -0800

    fixed some hard coded limits, added default standards

--- qPCR_analysis.py
+++ qPCR_analysis.py
@@ -9,6 +9,7 @@ import numpy as np
 import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as mplpatches
+import matplotlib.cm as cm
 
 # stylesheet for graphs
 plt.style.use('bw_graph.mplstyle')
@@ -56,35 +57,48 @@ def generateStandards (mean_dict):
             #standards_dict[mean_dict_keys[i]] =
     #for i in range(0,len(standards_dict))
     #print (gene_standards, housekeeping_standards)
+    print ('here are the standards')
+    print (gene_standards)
+    print (housekeeping_standards)
+
+    HLNC1_standards = [(-0.69897000433601875, 25.29866663614909), (-2.6989700043360187, 33.68900108337402), (0.0, 22.18500010172526), (-1.6989700043360187, 30.017666498819988)]
+    ACTB_standards = [(-0.69897000433601875, 26.45466677347819), (-1.6989700043360187, 30.544666926066082), (-2.6989700043360187, 34.641000747680664), (0.0, 23.806000391642254)]
+
+    if not gene_standards:
+        gene_standards = HLNC1_standards
+    if not housekeeping_standards:
+        housekeeping_standards = ACTB_standards
 
     gene_standards_y,gene_standards_x = zip(*gene_standards)
     housekeeping_standards_y,housekeeping_standards_x = zip(*housekeeping_standards)
+
     #housekeeping_standards_x = [x[0] for x in housekeeping_standards]
 
+    # use numpy.polyfit to fit a linear curve to standards
     gene_standards_coefficients = np.polyfit(gene_standards_x,gene_standards_y,1)
     housekeeping_standards_coefficients = np.polyfit(housekeeping_standards_x,housekeeping_standards_y,1)
 
+    # set
     x_pos = np.arange(20,35,0.25)
     g_y_pos = gene_standards_coefficients[0] * x_pos + gene_standards_coefficients[1]
     hk_y_pos = housekeeping_standards_coefficients[0] * x_pos + housekeeping_standards_coefficients[1]
-    plt.figure(1,figsize=(2,2))
 
-    panel1=plt.axes([0.2,0.2,1.5/2,1.5/2])
-    #panel2=plt.axes([2.1,0.15,1.5/4,1.5/2])
+    fig_1 = plt.figure(figsize=(4,2))
+    panel1=plt.axes([0.1,0.2,1.5/4,1.5/2])
+    panel2=plt.axes([0.6,0.2,1.5/4,1.5/2])
 
     panel1.scatter(gene_standards_x,gene_standards_y, \
                    #s=np.array(gene_length_list)/1000,\
-                   s=8,\
+                   s=20,\
                    marker='.',\
                    alpha=1,\
                    facecolor=(0,0,0),\
                    edgecolor='none',\
                    linewidth=0.1)
 
-
-    panel1.scatter(housekeeping_standards_x,housekeeping_standards_y, \
+    panel2.scatter(housekeeping_standards_x,housekeeping_standards_y, \
                    #s=np.array(gene_length_list)/1000,\
-                   s=8,\
+                   s=20,\
                    marker='.',\
                    alpha=1,\
                    facecolor=(0,0,0),\
@@ -92,24 +106,26 @@ def generateStandards (mean_dict):
                    linewidth=0.1)
 
     panel1.plot(x_pos,g_y_pos,alpha = 0.5,label='HLNC1',color='blue')
-    panel1.plot(x_pos,hk_y_pos,alpha = 0.5, label='ACTB',color='orange')
-    plt.legend(fontsize = 4)
+    panel2.plot(x_pos,hk_y_pos,alpha = 0.5, label='ACTB',color='orange')
+
+    panel1.legend(fontsize = 4)
+    panel2.legend(fontsize = 4)
     panel1.set_xlabel('CT')
-    panel1.set_ylabel('log10 (dilution factor)')
+    panel1.set_ylabel('log$\mathregular{_{10}}$ (dilution factor)')
     gene = 'y = '+str(round(gene_standards_coefficients[0],4))+ ' * x + ' + str(round(gene_standards_coefficients[1],4))
     hk = 'y = '+str(round(housekeeping_standards_coefficients[0],4))+ ' * x + ' + str(round(housekeeping_standards_coefficients[1],4))
     panel1.text(22,-2,gene,color = 'blue',fontsize=4)
-    panel1.text(26,0,hk,color = 'orange',fontsize=4)
+    panel2.text(26,0,hk,color = 'orange',fontsize=4)
     panel1.tick_params(axis='both',which='both',\
                        bottom='on', labelbottom='on',\
                        left='on', labelleft='on',\
                        right='off', labelright='off',\
                        top='off', labeltop='off')
-    """panel2.tick_params(axis='both',which='both',\
+    panel2.tick_params(axis='both',which='both',\
                        bottom='on', labelbottom='on',\
                        left='on', labelleft='on',\
                        right='off', labelright='off',\
-                       top='off', labeltop='off')"""
+                       top='off', labeltop='off')
 
     plt.savefig('standards_graphs.png')
 
@@ -156,7 +172,7 @@ def normalize (qPCR_excel):
     sum_score = 0
     normalized_score_list = list()
     sample_dict = dict()
-    norm_score_array = np.ones(96)
+    norm_score_array = np.ones(len(qPCR_excel))
 
     for index, row in sorted_excel.iterrows():
         if count >= (len(qPCR_excel)/2):
@@ -167,10 +183,10 @@ def normalize (qPCR_excel):
         sample_name = row['Sample Name']
         #print (sample_name)
         housekeeping_score = sorted_excel.iloc[[count]]['10^RVQ'].values
-        gene_score = sorted_excel.iloc[[count+48]]['10^RVQ'].values
+        gene_score = sorted_excel.iloc[[count+len(qPCR_excel)/2]]['10^RVQ'].values
         normalized_score = gene_score / housekeeping_score
 
-        norm_score_array[count+48]=(float(normalized_score))
+        norm_score_array[count+len(qPCR_excel)/2]=(float(normalized_score))
         count += 1
         if old_name == sample_name:
             #print (row['Sample Name'])
@@ -224,26 +240,48 @@ def printGraphs (normalized_scores):
     #panel3=plt.axes([4.15,0.2,3.75/15,3.75/5])
     #panel4=plt.axes([6.15,0.2,3.75/15,3.75/5])
     name_list = list()
+    group_list = list()
     mean_list = list()
+
     for tup in normalized_scores:
         #print ('yo')
         name = tup[0].split()[0] + ' ' + tup[0].split()[1]
-        name_list.append(name)
+
+
         sample_array = np.array(list(tup[1].values()))
-        #print (sample_array)
+        # find the mean and disregard missing points
         mean_score = np.nanmean(sample_array)
-        if mean_score:
-            mean_list.append(mean_score)
+        if not ('NCCIT' in name):
+            if mean_score:
+                name_list.append(name)
+                group_list.append(name.split()[0])
+                mean_list.append(mean_score)
     print (mean_list)
-    #print (name_list)
+    print (name_list)
 
     locations = np.arange(len(mean_list))
 
+    unique_groups = set(group_list)
+    unique_groups_list = sorted(list(unique_groups) )            # == set(['a', 'b', 'c'])
+    unique_groups_count = len(unique_groups)
+    #print (unique_groups_list)
+    #print (name_list)
+    #print (unique_groups_count)
+    #plt.jet()
+    #plt.set_cmap('jet')
+    colors_subsection = np.linspace(0, 1, len(unique_groups))
+    colors = [cm.jet(x) for x in colors_subsection]
+    #print (colors)
+
     count = 0
-    for i in range(0,len(mean_list)-4):
+    group_count = 0
+    temp_group = unique_groups_list[0]
+
+    for i in range(0,len(mean_list)):
         left = locations[i]-.35
         width = 0.7
         height = mean_list[i]
+        #name
         """
         if count < 2:
             rectangle1=mplpatches.Rectangle((left,bottom),width,height,\
@@ -258,16 +296,26 @@ def printGraphs (normalized_scores):
                                         edgecolor=(0,0,0))
             count = 0
         """
+        # iterate through group colors
+        if not (unique_groups_list[group_count] in name_list[i]):
+            group_count += 1
+
         rectangle1=mplpatches.Rectangle((left,0),width,height,\
                                     linewidth=0.1,\
-                                    facecolor=(0.5,0.5,0.5),\
+                                    facecolor=colors[group_count],\
                                     edgecolor=(0,0,0))
         panel1.add_patch(rectangle1)
 
-    panel1.set_xlim([5,9])
-    panel1.set_ylim([0,1])
-    panel1.xaxis.set_ticks(np.arange(6,len(mean_list)-7,1))
-    panel1.set_xticklabels(name_list[6:10],rotation=90)
+    #panel1.set_xlim([-1,9])
+    #panel1.set_ylim([0,1])
+    #panel1.xaxis.set_ticks(np.arange(6,len(mean_list)-7,1))
+    #panel1.set_xticklabels(name_list[6:10],rotation=90)
+
+
+    panel1.set_xlim([-1,len(mean_list)])
+    panel1.set_ylim([0,0.15])
+    panel1.xaxis.set_ticks(np.arange(0,len(mean_list),1))
+    panel1.set_xticklabels(name_list,rotation=90)
     #panel1.set_xticks([name_list])
     #panel1.margins(0.25)
 
@@ -281,6 +329,52 @@ def printGraphs (normalized_scores):
 
     plt.savefig('HLNC1-2_normalized_graphs.png')
 
+"""
+class CommandLine() :
+    '''
+    modified from David Bernick
+
+    Handle the command line, usage and help requests.
+
+    CommandLine uses argparse, now standard in 2.7 and beyond.
+    it implements a standard command line argument parser with various argument options,
+    a standard usage and help, and an error termination mechanism do-usage_and_die.
+
+    attributes:
+    myCommandLine.args is a dictionary which includes each of the available command line arguments as
+    myCommandLine.args['option']
+    '''
+
+    def __init__(self, inOpts=None) :
+    '''
+    CommandLine constructor.
+    Implements a parser to interpret the command line argv string using argparse.
+    '''
+        import argparse
+
+        self.parser = argparse.ArgumentParser(description = 'Program prolog - Specify the <input and >output files, gene names, and standards.',
+                                              epilog = 'Program epilog - parameters of infile and outfile must be given.',
+                                              add_help = True, #default is True
+                                              prefix_chars = '-',
+                                              usage = '%(prog)s <infile >outfile -g | -h | -s'
+                                              )
+        self.parser.add_argument('-g', '--geneOfInterest', dest='gene_name',
+                                 default='HLNC1', action='store', type=str,
+                                 help='specify the gene of interest')
+        self.parser.add_argument('-h', '--housekeepingGene', default='ACTB',
+                                 dest='housekeeping_name', action='store', type=str,
+                                 help='specify the housekeeping gene')
+        self.parser.add_argument('-s', '--standard', default='standard',
+                                 dest='standard_name', action='store', type=str,
+                                 help='specify tag to identify standards')
+
+
+        if inOpts is None :
+            self.args = self.parser.parse_args()
+        else :
+            self.args = self.parser.parse_args(inOpts)
+
+"""
 
 def main():
 
@@ -311,32 +405,4 @@ sample_name_column = results_sheet.col_slice(colx=3,
 CT_column = results_sheet.col_slice(colx=8,
                                 start_rowx=36,
                                 end_rowx=132)
-"""
-
-#for cell in well_column:
-#    print (cell.value)
-
-#for cell in CT_column:
-    #print (cell.value)
-"""
-def generateMeanList ():
-    mean_list = list()
-    old_sample_name = sample_name_column[0].value
-    current_value_list = list()
-    for i in range(1,len(CT_column)):
-        if row['CT'] == 'Undetermined':
-            current_value = 'CT_column[i].value'
-        else:
-            current_value = CT_column[i].value
-        current_sample_name = sample_name_column[i].value
-        if old_sample_name == current_sample_name:
-            #current_value_list.append(float(current_value))
-            print (current_value)
-            old_sample_name = current_sample_name
-        else:
-            #mean_list.append(sum(current_value_list)/len(current_value_list))
-            current_value_list = [current_value]
-            old_sample_name = current_sample_name
-
-generateMeanList()
 """
